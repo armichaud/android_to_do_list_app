2aaa4fb6c205fd51d94487a614ddcf9554569d54
armichaud <email@alexandermichaud.tech>
2024-01-18 23:14:23 -0500

    Move daos to separate files

--- app/src/main/java/com/example/todolist/database/AppDatabase.kt
+++ app/src/main/java/com/example/todolist/database/AppDatabase.kt
@@ -1,21 +1,16 @@
 package com.example.todolist.database
 
 import android.content.Context
-import androidx.room.Dao
 import androidx.room.Database
-import androidx.room.Insert
-import androidx.room.Query
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.todolist.models.ListItem
-import com.example.todolist.utils.Status
 import com.example.todolist.models.ToDoList
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.flow.Flow
 
 @Database(entities = [ToDoList::class, ListItem::class], version = 1)
 abstract class AppDatabase: RoomDatabase() {
@@ -41,31 +36,3 @@ object DataBaseModule {
             .build()
     }
 }
-
-@Dao
-interface ListDao {
-    @Query("SELECT * FROM to_do_list")
-    fun getAll(): Flow<List<ToDoList>>
-
-    @Insert
-    fun addList(list: ToDoList): Long
-
-    @Query("SELECT * FROM to_do_list WHERE id = :id")
-    fun getList(id: Long): ToDoList
-}
-
-@Dao
-interface ListItemDao {
-    @Query("SELECT * FROM list_item WHERE list_id == :listId ORDER BY status DESC, id")
-    fun getAllForList(listId: Int): Flow<List<ListItem>>
-
-    @Insert
-    fun addListItem(listItem: ListItem)
-
-    @Query("DELETE FROM list_item WHERE id == :id")
-    fun deleteListItem(id: Int)
-
-    @Query("UPDATE list_item SET status = :status WHERE id = :id")
-    fun updateListItemStatus(id: Int, status: Status)
-}
-
---
+++ app/src/main/java/com/example/todolist/database/Daos.kt
@@ -0,0 +1,36 @@
+package com.example.todolist.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.todolist.models.ListItem
+import com.example.todolist.models.ToDoList
+import com.example.todolist.utils.Status
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ListDao {
+    @Query("SELECT * FROM to_do_list")
+    fun getAll(): Flow<List<ToDoList>>
+
+    @Insert
+    fun addList(list: ToDoList): Long
+
+    @Query("SELECT * FROM to_do_list WHERE id = :id")
+    fun getList(id: Long): ToDoList
+}
+
+@Dao
+interface ListItemDao {
+    @Query("SELECT * FROM list_item WHERE list_id == :listId ORDER BY status DESC, id")
+    fun getAllForList(listId: Int): Flow<List<ListItem>>
+
+    @Insert
+    fun addListItem(listItem: ListItem)
+
+    @Query("DELETE FROM list_item WHERE id == :id")
+    fun deleteListItem(id: Int)
+
+    @Query("UPDATE list_item SET status = :status WHERE id = :id")
+    fun updateListItemStatus(id: Int, status: Status)
+}
